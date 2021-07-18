820203b4f98be4fffdb7b9831717f79aa8b4fce1
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-07-18 15:53:01 -0700

    Add current date to the header 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
-// display current date to the header
-var currentDateHeader = moment().format("dddd, MMMM Do");
-var currentDayEl = $("#currentDay").text(currentDateHeader);
-console.log(currentDayEl);
+var day = (moment().format("DDDDYYYY"));
+var dayIncrement = 0;
+var hour = moment().hours();
+
+function displayDay() {
+    $(document).ready(function () {
+        // display current date to the header
+        var currentDateHeader = (moment().add(dayIncrement, 'd')).format("dddd, MMMM Do");
+        $("#currentDay").text(currentDateHeader);
+        var taskDateFormat = (moment().add(dayIncrement, 'd')).format("DDDDYYYY");
+        var taskArr = JSON.parse(localStorage.getItem(taskDateFormat));
+        if (taskArr) {
+            arrOne = taskArr;
+        } else {
+            var arrOne = [];
+        };
+        $('body').append($('<div>').prop({ id: "container", className: "container" })
+        );
+        // container
+        var hourEl = document.getElementById("container");
+        while (hourEl.hasChildNodes()) {
+            hourEl.removeChild(hourEl.firstChild);
+        };
+    })
+};
+// call the function
+displayDay();
